ff2328f6e9cba67280dd62ae317942e1faef283b
CymatiCorp <warrenarea@gmail.com>
2017-12-23 19:23:20 -0800

    Update CyKITv2.py

--- Python/CyKITv2.py
+++ Python/CyKITv2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 #
-# CyKIT v2 - 2017.11
-# ====================
+# CyKIT v2 - 2017.12.15
+# =======================
 # Written by Warren
 #
 
@@ -16,29 +16,35 @@ import threading
 arg_count = len(sys.argv)
 
 if arg_count == 1 or arg_count > 5 or sys.argv[1] == "help" or sys.argv[1] == "--help" or sys.argv[1] == "/?":
-    print ""
+    print "\r\n"
+    print " (Version: CyKITv2:2017.12.15) -- Python 2.7.6 on Win32 \r\n"
     print "\r\n Usage:  Python.exe CyKITv2.py <IP> <Port> <Model#(1-6)> [config] \r\n"
-    print " _________________________________________________________________________\r\n"
+    print " " + "_" * 85 + "\r\n"
     print " <IP> <PORT> for CyKIT to listen on. \r\n" 
-    print " _________________________________________________________________________\r\n"
+    print " " + "_" * 85 + "\r\n"
     print " <Model#> Choose the decryption type. \r\n"
-    print "          1 - Epoc (Research\r\n"
+    print "          1 - Epoc (Research)\r\n"
     print "          2 - Epoc (Standard)\r\n"
     print "          3 - Insight (Research)\r\n"
     print "          4 - Insight (Standard)\r\n"
     print "          5 - Epoc+ (Research)\r\n"
     print "          6 - Epoc+ (Standard)\r\n\r\n"
-    print " _________________________________________________________________________\r\n"
+    print " " + "_" * 85 + "\r\n"
     print " [config] is optional. \r\n"
     print "  'info' prints additional information into console.\r\n"
-    print "  'confirm' requests you to confirm a device everytime device is initialized.\r\n"
-    print "   Join these words together, using a separator. \r\n"
+    print "  'confirm'  Requests you to confirm a device everytime device is initialized.\r\n"
+    print "  'nocounter'  Removes all counters from 'all' outputs.\r\n"
+    print "  'format-0' (Default) Outputs 14 data channels in float format. ('4201.02564096') \r\n"
+    print "  'format-1' Outputs the raw data (to be converted by Javascript or other). \r\n"
+    print "  'outputdata'  Prints the (formatted) data being sent, to the console window.\r\n"
+    print "  'outputencrypt'  Prints the (encrypted) rjindael data to the console window.\r\n\r\n"
+    print "   Join these words together, using a + separator. \r\n"
     print "   (e.g  info+confirm ) \r\n\r\n"
-    print " _________________________________________________________________________\r\n"
-    print " Example Usage: \r\n"
-    print " Python.exe CyKITv2.py 127.0.0.1 55555 2 info+confirm \r\n\r\n"
-    print " _________________________________________________________________________\r\n"
-    sys.argv = [sys.argv[0], "127.0.0.1", "55555", "2", ""]
+    print " " + "_" * 85 + "\r\n"
+    print "  Example Usage: \r\n"
+    print "  Python.exe CyKITv2.py 127.0.0.1 55555 1 info+confirm \r\n\r\n"
+    print " " + "_" * 85 + "\r\n"
+    sys.argv = [sys.argv[0], "127.0.0.1", "55555", "1", ""]
     
     
 if arg_count < 5:
@@ -99,7 +105,7 @@ def main(CyINIT):
     
     while CyINIT > 2:
         CyINIT += 1
-
+        
         if CyINIT > 100:
             modelCheck = myi.modelChange()
             if modelCheck != 0:
@@ -110,6 +116,7 @@ def main(CyINIT):
             for t in threading.enumerate():
                 if t.getName() == "ioThread" or t.getName() == "eegThread":
                     check_threads += 1
+            
             if check_threads == 1:
                 ioTHREAD.onClose()
                 print "*** Reseting . . ."
